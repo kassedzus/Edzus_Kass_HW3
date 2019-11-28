127509ddfa5a1ea25085701960514ba1670ece92
Edzus Kass <kassedzus@gmail.com>
2019-11-28 14:27:58 +0200

    added a genDivs function and some minor changes

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -10,6 +10,8 @@ const restartBtn = document.getElementById("btn2");
 const fizzBtn = document.getElementById("btn3");
 const buzzBtn = document.getElementById("btn4");
 const fizzbuzzBtn = document.getElementById("btn5");
+const buttons = document.querySelector(".buttons");
+
 
 function clearCont() {
     while (container.firstChild) {
@@ -20,7 +22,7 @@ function clearCont() {
 // WARNING MESSAGE
 const warning = document.createElement("p");
 warning.setAttribute("id", "warning");
-warning.innerText = "Warning! Numbers can't be negative. Number range is between 0 and 100. Second number can't be smaller than first number";
+warning.innerText = "Warning! Number range is between 1 and 100. Second number can't be smaller than first number";
 
 // function clearWarn() {
 //     let node = document.getElementById("warning");
@@ -29,14 +31,7 @@ warning.innerText = "Warning! Numbers can't be negative. Number range is between
 //     }
 // }
 
-// GENERATE BUTTON - GENERATES DIVS BASED ON INPUT NUMBERS AND SORTS THEM (FIZZ, BUZZ or FIZZBUZZ or just a number)
-gen.onclick = () => {
-    console.log("Start number is: " + firstNum.value);
-    console.log("End number is: " + secondNum.value);
-    console.log("Generating divs...");
-    clearCont();
-    // clearWarn();
-
+function genDivs() {
     for (let i = parseInt(firstNum.value); i <= parseInt(secondNum.value); i++) {
         let fN = parseInt(firstNum.value);
         let sN = parseInt(secondNum.value);
@@ -46,8 +41,8 @@ gen.onclick = () => {
 
         const numDiv = document.createElement("div")
 
-        if (sN < fN || sN > 100 || fN < 0 || sN < 0) {
-            head.appendChild(warning);
+        if (sN < fN || sN > 100 || fN <= 0 || sN <= 0) {
+            buttons.appendChild(warning);
             break;
         } else if (i % 3 == 0 && i % 5 == 0) {
             container.appendChild(numDiv);
@@ -67,6 +62,49 @@ gen.onclick = () => {
             numDiv.setAttribute("class", "number")
         }
     }
+
+}
+
+// GENERATE BUTTON - GENERATES DIVS BASED ON INPUT NUMBERS AND SORTS THEM (FIZZ, BUZZ or FIZZBUZZ or just a number)
+gen.onclick = () => {
+    console.log("Start number is: " + firstNum.value);
+    console.log("End number is: " + secondNum.value);
+    console.log("Generating divs...");
+
+    clearCont();
+    genDivs();
+    // clearWarn();
+
+    // for (let i = parseInt(firstNum.value); i <= parseInt(secondNum.value); i++) {
+    //     let fN = parseInt(firstNum.value);
+    //     let sN = parseInt(secondNum.value);
+    //     // console.log("First num is:" + fN + " Second num is " + sN);
+
+
+
+    //     const numDiv = document.createElement("div")
+
+    //     if (sN < fN || sN > 100 || fN < 0 || sN < 0) {
+    //         head.appendChild(warning);
+    //         break;
+    //     } else if (i % 3 == 0 && i % 5 == 0) {
+    //         container.appendChild(numDiv);
+    //         numDiv.innerText = i + " = FIZZ-BUZZ";
+    //         numDiv.setAttribute("class", "fizzbuzz");
+    //     } else if (i % 3 == 0) {
+    //         container.appendChild(numDiv);
+    //         numDiv.innerText = i + " = FIZZ";
+    //         numDiv.setAttribute("class", "fizz");
+    //     } else if (i % 5 == 0) {
+    //         container.appendChild(numDiv);
+    //         numDiv.innerText = i + " = BUZZ";
+    //         numDiv.setAttribute("class", "buzz");
+    //     } else {
+    //         container.appendChild(numDiv);
+    //         numDiv.innerText = i;
+    //         numDiv.setAttribute("class", "number")
+    //     }
+    // }
 }
 
 // CLEAR AND RESTART BUTTONS
@@ -77,11 +115,16 @@ clearBtn.onclick = () => {
 }
 
 restartBtn.onclick = () => {
+    clearCont();
+    firstNum.value = 1;
+    secondNum.value = 100;
+    genDivs();
 
 }
 
 // FIZZ, BUZZ and FIZZBUZZ BUTTONS
 fizzBtn.onclick = () => {
+    console.log("Showing only FIZZ numbers");
     for (let el of document.querySelectorAll('div.buzz, div.fizzbuzz, div.number')) {
         el.style.display = 'none';
     }
@@ -90,6 +133,7 @@ fizzBtn.onclick = () => {
     }
 }
 buzzBtn.onclick = () => {
+    console.log("Showing only BUZZ numbers");
     for (let el of document.querySelectorAll('div.fizz, div.fizzbuzz, div.number')) {
         el.style.display = 'none';
     }
@@ -98,6 +142,7 @@ buzzBtn.onclick = () => {
     }
 }
 fizzbuzzBtn.onclick = () => {
+    console.log("Showing only FIZZ-BUZZ numbers");
     for (let el of document.querySelectorAll('div.buzz, div.fizz, div.number')) {
         el.style.display = 'none';
     }
@@ -105,3 +150,5 @@ fizzbuzzBtn.onclick = () => {
         el.style.display = "block";
     }
 }
+
+// UZTAISĪT CIKLU KAS ĢENERĒ DIVUS UN FIZZBUZZUS KĀ FUNKCIJU UN TAD IEVIETOT GAN BUTTON.ONCLICK GAN RESTART POGAI.
